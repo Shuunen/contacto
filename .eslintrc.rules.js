c05d8a21b222e788f1ebf6e09de359aa34dc176a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-03-28 17:33:37 +0200

    setup github ci

--- .eslintrc.rules.js
+++
@@ -1,8 +0,0 @@
-/*
-  Custom rules for this project
-*/
-module.exports = {
-  'import/export': 'off',
-  'vue/max-attributes-per-line': 'off',
-  'vue/singleline-html-element-content-newline': 'off',
-}
